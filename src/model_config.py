7c5747c715e892e8eaf501847b8c32fe7a81aeca
vikdevit <v.khatcherian@outlook.fr>
2026-07-19 14:46:04 +0200

    FEAT: model and threshold loading optimisation

--- src/model_config.py
+++ src/model_config.py
@@ -1,5 +1,27 @@
+# Ci-dessous avant optimisation par mise en cache du seuil de décision
+#import json
+#from pathlib import Path
+#
+#
+#THRESHOLD_PATH = Path(
+#    "artifacts/model/threshold.json"
+#)
+#
+#
+#def load_threshold():
+#
+#    with open(
+#        THRESHOLD_PATH,
+#        "r"
+#    ) as f:
+#        data = json.load(f)
+#
+#    return data["threshold"]
+
+# ci-desssous version avec optimisation par mise en cache du seuil de décision
 import json
 from pathlib import Path
+from functools import lru_cache
 
 
 THRESHOLD_PATH = Path(
@@ -7,6 +29,7 @@ THRESHOLD_PATH = Path(
 )
 
 
+@lru_cache(maxsize=1)
 def load_threshold():
 
     with open(
